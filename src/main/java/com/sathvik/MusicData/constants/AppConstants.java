a27e2168ce5b6634feb7ad1a134907e558930a04
sathvikpoturaju <sathvikpoturaju@gmail.com>
2024-09-23 23:46:37 +0530

    Update AppConstants.java

--- src/main/java/com/sathvik/MusicData/constants/AppConstants.java
+++ src/main/java/com/sathvik/MusicData/constants/AppConstants.java
@@ -4,7 +4,7 @@ public class AppConstants {
     public static final String API_SECRET_KEY = "b5Yto8N0IJUUsaspslPJ4B7EFCuFTsAswBkGT82XtmnJWlsLYtZZn1GYi_p06e36Uu9AoH__inR03NafDQ3dWA";
     public static final String[] ALLOWED_ORIGINS = new String[] {
             "http://localhost:3000",
-            "https://keepaliveapp.onrender.com"
+            "https://keepalive-b6i1.onrender.com"
     };
-    public static final String KEEP_ALIVE_SERVER_URL = "https://keepaliveapp.onrender.com";
+    public static final String KEEP_ALIVE_SERVER_URL = "https://keepalive-b6i1.onrender.com";
 }
