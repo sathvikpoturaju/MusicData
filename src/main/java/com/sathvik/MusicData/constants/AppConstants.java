7d61e4a210d78d1f4ed4dd943b20ebffdb387362
sathvikpoturaju <sathvikpoturaju@gmail.com>
2024-09-24 00:00:52 +0530

    Update AppConstants.java

--- src/main/java/com/sathvik/MusicData/constants/AppConstants.java
+++ src/main/java/com/sathvik/MusicData/constants/AppConstants.java
@@ -3,7 +3,7 @@ package com.sathvik.MusicData.constants;
 public class AppConstants {
     public static final String API_SECRET_KEY = "b5Yto8N0IJUUsaspslPJ4B7EFCuFTsAswBkGT82XtmnJWlsLYtZZn1GYi_p06e36Uu9AoH__inR03NafDQ3dWA";
     public static final String[] ALLOWED_ORIGINS = new String[] {
-            "http://localhost:3000",
+            "https://pianimation-music-links.netlify.app",
             "https://keepalive-b6i1.onrender.com"
     };
     public static final String KEEP_ALIVE_SERVER_URL = "https://keepalive-b6i1.onrender.com";
